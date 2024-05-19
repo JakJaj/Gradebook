d0f48a33aa32a4cf31b9014690470c6d56ffd47c
Jakub Jajonek <jakub.jajonek@gmail.com>
2024-05-20 00:07:41 +0200

    add not nulls for each key

--- sql/create/create.sql
+++ sql/create/create.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE IF NOT EXISTS `grade_book`;
 USE `grade_book`;
 
-DROP TABLE IF EXISTS Messeges;
+DROP TABLE IF EXISTS Messages;
 DROP TABLE IF EXISTS Notes;
 DROP TABLE IF EXISTS Grades;
 DROP TABLE IF EXISTS Students_Parents;
@@ -34,7 +34,8 @@ CREATE TABLE Teachers(
     date_of_birth DATE,
     email VARCHAR(255),
     date_of_employment DATE,
-    user_id BIGINT,
+    user_id BIGINT NOT NULL,
+    
     PRIMARY KEY(teacher_id),
     FOREIGN KEY(user_id) REFERENCES Users(user_id)
 );
@@ -45,7 +46,7 @@ CREATE TABLE Parents(
     first_name VARCHAR(255),
     last_name VARCHAR(255),
     email VARCHAR(255),
-    user_id BIGINT,
+    user_id BIGINT NOT NULL,
     
     PRIMARY KEY (parent_id),
     FOREIGN KEY (user_id) REFERENCES Users(user_id)
@@ -56,7 +57,7 @@ CREATE TABLE Announcements(
     title VARCHAR(255),
     content TEXT,
     date_time DATETIME,
-    teacher_id BIGINT,
+    teacher_id BIGINT NOT NULL,
     PRIMARY KEY (announcement_id),
     FOREIGN KEY (teacher_id) REFERENCES Teachers(teacher_id)
 );
@@ -71,8 +72,8 @@ CREATE TABLE Course_Types(
 
 CREATE TABLE Courses(
 	course_id BIGINT NOT NULL,
-    course_type_id BIGINT,
-    teacher_id BIGINT,
+    course_type_id BIGINT NOT NULL,
+    teacher_id BIGINT NOT NULL,
     description VARCHAR(255),
     
     PRIMARY KEY(course_id),
@@ -84,7 +85,7 @@ CREATE TABLE Courses(
 CREATE TABLE Classes(
 	class_id BIGINT NOT NULL,
     class_name VARCHAR(255),
-    teacher_id BIGINT,
+    teacher_id BIGINT NOT NULL,
     start_year SMALLINT,
     
     PRIMARY KEY(class_id),
@@ -101,8 +102,8 @@ CREATE TABLE Students(
     city VARCHAR(255),
     street VARCHAR(255),
     house_number INT,
-    class_id BIGINT,
-    user_id BIGINT,
+    class_id BIGINT NOT NULL,
+    user_id BIGINT NOT NULL,
     active BOOL,
     
     PRIMARY KEY(student_id),
@@ -123,8 +124,8 @@ CREATE TABLE Students_Parents(
 -- Tworzenie tabeli Timetables
 CREATE TABLE Timetables(
 	timetable_id BIGINT NOT NULL,
-    course_id BIGINT,
-    class_id BIGINT,
+    course_id BIGINT NOT NULL,
+    class_id BIGINT NOT NULL,
     start_time TIME,
     end_time TIME,
     classroom_number VARCHAR(255),
@@ -138,9 +139,9 @@ CREATE TABLE Timetables(
 -- Tworzenie tabeli Grades
 CREATE TABLE Grades(
 	grade_id BIGINT NOT NULL,
-    course_id BIGINT,
+    course_id BIGINT NOT NULL,
     mark INT,
-    student_id BIGINT,
+    student_id BIGINT NOT NULL,
     description TEXT,
     magnitude INT,
     
@@ -154,8 +155,8 @@ CREATE TABLE Attendances(
 	attendance_id BIGINT NOT NULL,
     date_time DATETIME,
     status VARCHAR(255),
-    student_id BIGINT,
-    timetable_id BIGINT,
+    student_id BIGINT NOT NULL,
+    timetable_id BIGINT NOT NULL,
     
     PRIMARY KEY(attendance_id),
     FOREIGN KEY(student_id) REFERENCES Students(student_id),
@@ -164,8 +165,8 @@ CREATE TABLE Attendances(
 -- Tworzenie tabeli Notes
 CREATE TABLE Notes(
 	note_id BIGINT NOT NULL,
-    student_id BIGINT,
-    timetable_id BIGINT,
+    student_id BIGINT NOT NULL,
+    timetable_id BIGINT NOT NULL,
     description text,
     date_time DATETIME,
     
@@ -175,9 +176,9 @@ CREATE TABLE Notes(
 );
 -- Tworzenie tabeli Messages
 CREATE TABLE Messages(
-	message_id BIGINT,
-    sender_id BIGINT,
-    receiver_id BIGINT,
+	message_id BIGINT NOT NULL,
+    sender_id BIGINT NOT NULL,
+    receiver_id BIGINT NOT NULL,
     title VARCHAR(255),
     content TEXT,
     date_time DATETIME,
