2b6467358001e167a040d3fc4467c35190125127
Jakub Jajonek <jakub.jajonek@gmail.com>
2024-05-19 20:23:02 +0200

    add generated spring boot project

---
+++ src/main/java/com/jj/Gradebook/GradebookApplication.java
@@ -0,0 +1,13 @@
+package com.jj.Gradebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradebookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jj/Gradebook/GradebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jj.Gradebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
