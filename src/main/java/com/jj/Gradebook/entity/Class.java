7eeff081d11ae6ed0a12492cb2407d49c97ad81a
Jakub Jajonek <jakub.jajonek@gmail.com>
2024-05-31 19:51:17 +0200

    add class entity fields

--- src/main/java/com/jj/Gradebook/entity/Class.java
+++ src/main/java/com/jj/Gradebook/entity/Class.java
@@ -1,4 +1,27 @@
 package com.jj.Gradebook.entity;
 
+
+import jakarta.persistence.*;
+
+import java.time.Year;
+
+@Entity
+@Table(name = "Classes")
 public class Class {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "class_id")
+    private int classId;
+
+    @Column(name = "class_name")
+    private String className;
+
+    @Column(name = "teacher_id")
+    private String teacherId;
+
+    @Column(name = "start_year")
+    private Year start_year;
+
+    
 }
