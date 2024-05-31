29ce303ce727c010da68862fd002e1f45ffb4134
Jakub Jajonek <jakub.jajonek@gmail.com>
2024-05-31 19:31:57 +0200

    add constructors getters setters and toString to Users entity

--- src/main/java/com/jj/Gradebook/entity/Users.java
+++ src/main/java/com/jj/Gradebook/entity/Users.java
@@ -20,6 +20,62 @@ public class Users {
     @Column(name="role")
     private String role;
 
-}
+    public Users() {
+    }
 
+    public Users(String password, boolean enabled, String role) {
+        this.password = password;
+        this.enabled = enabled;
+        this.role = role;
+    }
+
+    public Users(int id, String password, boolean enabled, String role) {
+        this.id = id;
+        this.password = password;
+        this.enabled = enabled;
+        this.role = role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "Users{" +
+                "id=" + id +
+                ", password='" + password + '\'' +
+                ", enabled=" + enabled +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
 
