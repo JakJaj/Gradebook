35722a34679ded6b70d529a095c3b50cf52ab296
Jakub Jajonek <jakub.jajonek@gmail.com>
2024-05-31 20:31:27 +0200

    add announcement class fields

--- src/main/java/com/jj/Gradebook/entity/Announcements.java
+++ src/main/java/com/jj/Gradebook/entity/Announcements.java
@@ -1,4 +1,31 @@
 package com.jj.Gradebook.entity;
 
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Announcements")
 public class Announcements {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "announcement_id")
+    private int announcementId;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "date_time")
+    private LocalDateTime dateTime;
+
+    @Column(name = "teacher_id")
+    private int teacher_id;
+
+    
+
 }
