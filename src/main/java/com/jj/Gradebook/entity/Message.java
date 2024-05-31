6346bd112cd7b20df798d6ca64f0bef8de35833e
Jakub Jajonek <jakub.jajonek@gmail.com>
2024-05-31 21:54:42 +0200

    add message entity class with fields

---
+++ src/main/java/com/jj/Gradebook/entity/Message.java
@@ -0,0 +1,33 @@
+package com.jj.Gradebook.entity;
+
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Messages")
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "message_id")
+    private int messageId;
+
+    @Column(name = "sender_id")
+    private int senderId;
+
+    @Column(name = "receiver_id")
+    private int receiver_id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "date_time")
+    private LocalDateTime dateTime;
+
+    
+}
