d0cfb9da33748b777226ffc787ff5b1d887269af
Jakub Jajonek <jakub.jajonek@gmail.com>
2024-05-31 19:20:20 +0200

    add inserts for classes

--- sql/insert/insert.sql
+++ sql/insert/insert.sql
@@ -8,3 +8,8 @@ INSERT INTO Teachers (first_name, last_name, date_of_birth, email, date_of_emplo
 INSERT INTO Teachers (first_name, last_name, date_of_birth, email, date_of_employment, user_id) VALUES("Test2", "Nauczyciel2", STR_TO_DATE('02/10/1994', '%d/%m/%Y'), "test2@nauczyciel.com", STR_TO_DATE('12/11/2016', '%d/%m/%Y'), 2);
 INSERT INTO Teachers (first_name, last_name, date_of_birth, email, date_of_employment, user_id) VALUES("Test3", "Nauczyciel3", STR_TO_DATE('03/11/1995', '%d/%m/%Y'), "test3@nauczyciel.com", STR_TO_DATE('13/12/2017', '%d/%m/%Y'), 3);
 
+-- Wstawianie class do do ktorych przypisywani sa wychowacy z teachers
+INSERT INTO Classes (class_name, teacher_id, start_year) VALUES("IVD", 1, 2020);
+INSERT INTO Classes (class_name, teacher_id, start_year) VALUES("VE", 2, 2021);
+INSERT INTO Classes (class_name, teacher_id, start_year) VALUES("VIF", 3, 2022);
+
