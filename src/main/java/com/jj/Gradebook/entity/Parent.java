7405910757f6d7226fa99afb73d096c26829680c
Jakub Jajonek <jakub.jajonek@gmail.com>
2024-05-31 19:48:01 +0200

    add parent getters setters and toString

--- src/main/java/com/jj/Gradebook/entity/Parent.java
+++ src/main/java/com/jj/Gradebook/entity/Parent.java
@@ -42,5 +42,54 @@ public class Parent {
         this.userId = userId;
     }
 
-    
+    public int getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(int parentId) {
+        this.parentId = parentId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public String toString() {
+        return "Parent{" +
+                "parentId=" + parentId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", userId='" + userId + '\'' +
+                '}';
+    }
 }
