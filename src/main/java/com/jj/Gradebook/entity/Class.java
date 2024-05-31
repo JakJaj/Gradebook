cd49245298c953fc83edca1325ac4b44d36fd5a2
Jakub Jajonek <jakub.jajonek@gmail.com>
2024-05-31 20:05:54 +0200

    fix class class to use Lombok

--- src/main/java/com/jj/Gradebook/entity/Class.java
+++ src/main/java/com/jj/Gradebook/entity/Class.java
@@ -2,11 +2,19 @@ package com.jj.Gradebook.entity;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.Year;
 
 @Entity
 @Table(name = "Classes")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Class {
 
     @Id
@@ -23,61 +31,10 @@ public class Class {
     @Column(name = "start_year")
     private Year start_year;
 
-    public Class() {
-    }
-
-    public Class(int classId, String className, String teacherId, Year start_year) {
-        this.classId = classId;
-        this.className = className;
-        this.teacherId = teacherId;
-        this.start_year = start_year;
-    }
-
     public Class(String className, String teacherId, Year start_year) {
         this.className = className;
         this.teacherId = teacherId;
         this.start_year = start_year;
     }
 
-    public int getClassId() {
-        return classId;
-    }
-
-    public void setClassId(int classId) {
-        this.classId = classId;
-    }
-
-    public String getClassName() {
-        return className;
-    }
-
-    public void setClassName(String className) {
-        this.className = className;
-    }
-
-    public String getTeacherId() {
-        return teacherId;
-    }
-
-    public void setTeacherId(String teacherId) {
-        this.teacherId = teacherId;
-    }
-
-    public Year getStart_year() {
-        return start_year;
-    }
-
-    public void setStart_year(Year start_year) {
-        this.start_year = start_year;
-    }
-
-    @Override
-    public String toString() {
-        return "Class{" +
-                "classId=" + classId +
-                ", className='" + className + '\'' +
-                ", teacherId='" + teacherId + '\'' +
-                ", start_year=" + start_year +
-                '}';
-    }
 }
