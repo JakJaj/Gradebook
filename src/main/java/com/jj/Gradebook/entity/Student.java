260ed186b8e16b718d08ef14dd9e3d33515f6971
Jakub Jajonek <jakub.jajonek@gmail.com>
2024-05-31 20:02:12 +0200

    add student class fields

--- src/main/java/com/jj/Gradebook/entity/Student.java
+++ src/main/java/com/jj/Gradebook/entity/Student.java
@@ -1,4 +1,47 @@
 package com.jj.Gradebook.entity;
 
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "Students")
 public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "student_id")
+    private int studentId;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "date_of_birth")
+    private Date dateOfBirth;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "street")
+    private String street;
+
+    @Column(name = "house_number")
+    private int houseNumber;
+
+    @Column(name = "class_id")
+    private int classId;
+
+    @Column(name = "user_id")
+    private int userId;
+
+    @Column(name = "active")
+    private boolean active;
+    
 }
