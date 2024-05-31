d0937b692bac56d3216d2da137d4cc8265c1fb9b
Jakub Jajonek <jakub.jajonek@gmail.com>
2024-05-31 20:08:39 +0200

    fix user class to use Lombok

--- src/main/java/com/jj/Gradebook/entity/User.java
+++ src/main/java/com/jj/Gradebook/entity/User.java
@@ -1,9 +1,15 @@
 package com.jj.Gradebook.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Table(name="Users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class User {
 
     @Id
@@ -20,62 +26,10 @@ public class User {
     @Column(name="role")
     private String role;
 
-    public User() {
-    }
-
     public User(String password, boolean enabled, String role) {
         this.password = password;
         this.enabled = enabled;
         this.role = role;
-    }
-
-    public User(int userId, String password, boolean enabled, String role) {
-        this.userId = userId;
-        this.password = password;
-        this.enabled = enabled;
-        this.role = role;
-    }
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public boolean isEnabled() {
-        return enabled;
-    }
-
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + userId +
-                ", password='" + password + '\'' +
-                ", enabled=" + enabled +
-                ", role='" + role + '\'' +
-                '}';
     }
 }
 
