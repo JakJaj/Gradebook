6d454dabff2ef78a0c05d16a5416de750087b201
Jakub Jajonek <jakub.jajonek@gmail.com>
2024-05-31 19:41:32 +0200

    add teacher fields

--- src/main/java/com/jj/Gradebook/entity/Teacher.java
+++ src/main/java/com/jj/Gradebook/entity/Teacher.java
@@ -1,11 +1,36 @@
 package com.jj.Gradebook.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.util.Date;
 
 @Entity
 @Table(name="Teacher")
 public class Teacher {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="teacher_id")
+    private int teacherId;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="date_of_birth")
+    private Date dateOfBirth;
+
+    @Column(name="email")
+    private String email;
+
+    @Column(name="date_of_employment")
+    private Date dateOfEmployment;
+
+    @Column(name="user_id")
+    private int userId;
+
+    
 }
