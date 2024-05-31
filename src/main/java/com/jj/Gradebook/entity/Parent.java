c0879601f9fa9500f6b1a140cc125187cfb149c0
Jakub Jajonek <jakub.jajonek@gmail.com>
2024-05-31 19:46:11 +0200

    add parent class fields

--- src/main/java/com/jj/Gradebook/entity/Parent.java
+++ src/main/java/com/jj/Gradebook/entity/Parent.java
@@ -1,4 +1,28 @@
 package com.jj.Gradebook.entity;
 
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="Parents")
 public class Parent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="parent_id")
+    private int parentId;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name="user_id")
+    private String userId;
+
+    
 }
