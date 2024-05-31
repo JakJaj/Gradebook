125e6169fcf185f649a55d19a6149b4d14026c88
Jakub Jajonek <jakub.jajonek@gmail.com>
2024-05-31 19:21:00 +0200

    add inserts for parents

--- sql/insert/insert.sql
+++ sql/insert/insert.sql
@@ -23,3 +23,14 @@ INSERT INTO Students (first_name, last_name, email, date_of_birth, city, street,
 INSERT INTO Students (first_name, last_name, email, date_of_birth, city, street, house_number, class_id, user_id, active) VALUES("Test2", "Uczen2", "test2@uczen.com", STR_TO_DATE('02/02/2006', '%d/%m/%Y'), "Katowice", "Katowicka", 2, 2, 5, 0);
 INSERT INTO Students (first_name, last_name, email, date_of_birth, city, street, house_number, class_id, user_id, active) VALUES("Test3", "Uczen3", "test3@uczen.com", STR_TO_DATE('03/03/2007', '%d/%m/%Y'), "Chorzow", "Katowicka", 3, 3, 6, 0);
 
+
+-- Wstawianie rodzicow polaczonych z uzytkownikami o roli parent
+INSERT INTO Users (password, enabled, role) VALUES("Test1", 0, "PARENT");
+INSERT INTO Users (password, enabled, role) VALUES("Test2", 0, "PARENT");
+INSERT INTO Users (password, enabled, role) VALUES("Test3", 0, "PARENT");
+
+-- Wstawianie rodzicow polaczonych z uzytkownikami o roli parent
+INSERT INTO Parents (first_name, last_name, email, user_id) VALUES("Test1", "Rodzic1", "test1@rodzic.com", 7);
+INSERT INTO Parents (first_name, last_name, email, user_id) VALUES("Test2", "Rodzic2", "test2@rodzic.com", 8);
+INSERT INTO Parents (first_name, last_name, email, user_id) VALUES("Test3", "Rodzic3", "test3@rodzic.com", 9);
+
