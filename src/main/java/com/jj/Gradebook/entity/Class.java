55f1aafd68540a774b32df30c7d2107f279cfe15
Jakub Jajonek <jakub.jajonek@gmail.com>
2024-05-31 19:51:58 +0200

    add class constructors

--- src/main/java/com/jj/Gradebook/entity/Class.java
+++ src/main/java/com/jj/Gradebook/entity/Class.java
@@ -23,5 +23,19 @@ public class Class {
     @Column(name = "start_year")
     private Year start_year;
 
-    
+    public Class() {
+    }
+
+    public Class(int classId, String className, String teacherId, Year start_year) {
+        this.classId = classId;
+        this.className = className;
+        this.teacherId = teacherId;
+        this.start_year = start_year;
+    }
+
+    public Class(String className, String teacherId, Year start_year) {
+        this.className = className;
+        this.teacherId = teacherId;
+        this.start_year = start_year;
+    }
 }
