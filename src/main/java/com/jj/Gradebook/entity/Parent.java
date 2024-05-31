42d7589eb1ed1f709157512f51a229faa8feea96
Jakub Jajonek <jakub.jajonek@gmail.com>
2024-05-31 20:06:34 +0200

    fix parent class to use Lombok

--- src/main/java/com/jj/Gradebook/entity/Parent.java
+++ src/main/java/com/jj/Gradebook/entity/Parent.java
@@ -2,9 +2,17 @@ package com.jj.Gradebook.entity;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name="Parents")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Parent {
 
     @Id
@@ -24,17 +32,6 @@ public class Parent {
     @Column(name="user_id")
     private String userId;
 
-    public Parent() {
-    }
-
-    public Parent(int parentId, String firstName, String lastName, String email, String userId) {
-        this.parentId = parentId;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.userId = userId;
-    }
-
     public Parent(String firstName, String lastName, String email, String userId) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -42,54 +39,4 @@ public class Parent {
         this.userId = userId;
     }
 
-    public int getParentId() {
-        return parentId;
-    }
-
-    public void setParentId(int parentId) {
-        this.parentId = parentId;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    @Override
-    public String toString() {
-        return "Parent{" +
-                "parentId=" + parentId +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", email='" + email + '\'' +
-                ", userId='" + userId + '\'' +
-                '}';
-    }
 }
