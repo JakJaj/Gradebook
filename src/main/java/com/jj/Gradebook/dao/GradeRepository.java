04fa7726d1e6936b6acaa7753e65ae530477b619
Jakub Jajonek <jakub.jajonek@gmail.com>
2024-06-04 22:34:50 +0200

    add repository for grade

---
+++ src/main/java/com/jj/Gradebook/dao/GradeRepository.java
@@ -0,0 +1,7 @@
+package com.jj.Gradebook.dao;
+
+import com.jj.Gradebook.entity.Grade;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GradeRepository extends JpaRepository<Grade, Integer> {
+}
