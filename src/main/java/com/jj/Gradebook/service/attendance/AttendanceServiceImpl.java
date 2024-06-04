29f031caf11cd7d368b2da6e18cb82470b126aa8
Jakub Jajonek <jakub.jajonek@gmail.com>
2024-06-04 23:17:29 +0200

    add service with service implementation for attendance

---
+++ src/main/java/com/jj/Gradebook/service/attendance/AttendanceServiceImpl.java
@@ -0,0 +1,49 @@
+package com.jj.Gradebook.service.attendance;
+
+import com.jj.Gradebook.dao.AttendanceRepository;
+import com.jj.Gradebook.entity.Attendance;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AttendanceServiceImpl implements AttendanceService{
+    private final AttendanceRepository attendanceRepository;
+
+    public AttendanceServiceImpl(AttendanceRepository attendanceRepository) {
+        this.attendanceRepository = attendanceRepository;
+    }
+
+    @Override
+    public List<Attendance> findAll() {
+        return attendanceRepository.findAll();
+    }
+
+    @Override
+    public Attendance findById(int id) {
+        Optional<Attendance> result = attendanceRepository.findById(id);
+
+        Attendance attendance;
+        if(result.isPresent()){
+            attendance = result.get();
+        }
+        else{
+            throw new RuntimeException("No attendance with id - " + id);
+        }
+        return attendance;
+    }
+
+    @Override
+    @Transactional
+    public Attendance save(Attendance attendance) {
+        return attendanceRepository.save(attendance);
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(int id) {
+        attendanceRepository.deleteById(id);
+    }
+}
