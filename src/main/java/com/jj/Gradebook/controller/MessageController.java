c94afde79af10cd87213da2b35eaaeba5c54ad5f
Jakub Jajonek <jakub.jajonek@gmail.com>
2024-06-05 11:29:35 +0200

    add controller for messages

---
+++ src/main/java/com/jj/Gradebook/controller/MessageController.java
@@ -0,0 +1,39 @@
+package com.jj.Gradebook.controller;
+
+import com.jj.Gradebook.entity.Message;
+import com.jj.Gradebook.service.message.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class MessageController {
+    private MessageService messageService;
+
+    @Autowired
+    public MessageController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    @GetMapping("/messages")
+    public List<Message> findAll(){
+        return messageService.findAll();
+    }
+
+    @GetMapping("/messages/{id}")
+    public Message findById(@PathVariable int id){
+        return messageService.findById(id);
+    }
+
+    @PostMapping("/messages")
+    public Message save(@PathVariable Message message){
+        return messageService.save(message);
+    }
+
+    @DeleteMapping("/messages/{id}")
+    public void deleteById(@PathVariable int id){
+        messageService.deleteById(id);
+    }
+}
