760e4b267821b0fcefb1aecf115232b2f783d124
Jakub Jajonek <jakub.jajonek@gmail.com>
2024-06-05 11:00:23 +0200

    add controller for grades

---
+++ src/main/java/com/jj/Gradebook/controller/GradeController.java
@@ -0,0 +1,40 @@
+package com.jj.Gradebook.controller;
+
+import com.jj.Gradebook.entity.Grade;
+import com.jj.Gradebook.service.grade.GradeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class GradeController {
+
+    private GradeService gradeService;
+
+    @Autowired
+    public GradeController(GradeService gradeService) {
+        this.gradeService = gradeService;
+    }
+
+    @GetMapping("/grades")
+    public List<Grade> findAll(){
+        return gradeService.findAll();
+    }
+
+    @GetMapping("/grades/{id}")
+    private Grade findById(@PathVariable int id){
+        return gradeService.findById(id);
+    }
+
+    @PostMapping("/grades")
+    public Grade save(@PathVariable Grade grade){
+        return gradeService.save(grade);
+    }
+
+    @DeleteMapping("/grades/{id}")
+    public void deleteById(@PathVariable int id){
+        gradeService.deleteById(id);
+    }
+}
