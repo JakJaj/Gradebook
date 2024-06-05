55ae73f6f6bfdcc0cb669b8cd6fceff9f45183ce
Jakub Jajonek <jakub.jajonek@gmail.com>
2024-06-05 10:29:16 +0200

    add controller for users

--- src/main/java/com/jj/Gradebook/controller/UserController.java
+++ src/main/java/com/jj/Gradebook/controller/UserController.java
@@ -3,9 +3,7 @@ package com.jj.Gradebook.controller;
 import com.jj.Gradebook.entity.User;
 import com.jj.Gradebook.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +21,20 @@ public class UserController {
     @GetMapping("/users")
     public List<User> findAll(){
         return userService.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User findById(@PathVariable int id){
+        return userService.findById(id);
+    }
+
+    @PostMapping("/users") //TODO:TEST HOW TO INSERT USERS
+    public User save(@PathVariable User user){
+        return userService.save(user);
+    }
+
+    @DeleteMapping("/users/{id}") //FIRST YOU HAVE TO DELETE ELEMENTS WITH KEYS CONNECTED TO THIS USER
+    public void delete(@PathVariable int id){
+        userService.deleteById(id);
     }
 }
