ca4edff37e555efeab647ad2f93970e62ce33c78
Jakub Jajonek <jakub.jajonek@gmail.com>
2024-06-05 10:55:17 +0200

    add controller for teachers

---
+++ src/main/java/com/jj/Gradebook/controller/TeacherController.java
@@ -0,0 +1,40 @@
+package com.jj.Gradebook.controller;
+
+import com.jj.Gradebook.entity.Teacher;
+import com.jj.Gradebook.service.teacher.TeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class TeacherController {
+
+    private TeacherService teacherService;
+
+    @Autowired
+    public TeacherController(TeacherService teacherService) {
+        this.teacherService = teacherService;
+    }
+
+    @GetMapping("/teachers")
+    public List<Teacher> findAll(){
+        return teacherService.findAll();
+    }
+
+    @GetMapping("/teachers/{id}")
+    public Teacher findById(@PathVariable int id){
+        return teacherService.findById(id);
+    }
+
+    @PostMapping("/teacher")
+    public Teacher save(@PathVariable Teacher teacher){
+        return teacherService.save(teacher);
+    }
+
+    @DeleteMapping("/teacher/{id}")
+    private void deleteById(@PathVariable int id){
+        teacherService.deleteById(id);
+    }
+}
