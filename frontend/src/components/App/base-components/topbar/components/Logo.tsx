068ccf0393e912748729092c76115d63ccf9e580
Jakub Jajonek <jakub.jajonek@gmail.com>
2024-06-25 23:54:21 +0200

    fix class property name to className

--- frontend/src/components/App/base-components/topbar/components/Logo.tsx
+++ frontend/src/components/App/base-components/topbar/components/Logo.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function Logo(){
     return(
-        <a href="https://flowbite.com" class="flex items-center">
-            <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
-            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+        <a href="https://flowbite.com" className="flex items-center">
+            <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
         </a>
     );
 }
