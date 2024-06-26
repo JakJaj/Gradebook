174981a7a282b7bd92241921241a148e79fd65b5
Jakub Jajonek <jakub.jajonek@gmail.com>
2024-06-26 22:37:27 +0200

    create error page

---
+++ frontend/src/components/pages/error/ErrorPage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import TopBar from "../../App/base-components/topbar/TopBar";
+import Footer from "../../App/base-components/footer/Footer";
+import ErrorText from "./components/ErrorText";
+
+
+function ErrorPage() {
+  return (
+    <>
+    <ErrorText />
+    </>
+  );
+}
+
+export default ErrorPage;
