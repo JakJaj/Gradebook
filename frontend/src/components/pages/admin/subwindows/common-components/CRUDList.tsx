de33cca193c6839663d11295389997b3197e9062
Jakub Jajonek <jakub.jajonek@gmail.com>
2024-06-28 00:25:01 +0200

    design fix

--- frontend/src/components/pages/admin/subwindows/common-components/CRUDList.tsx
+++ frontend/src/components/pages/admin/subwindows/common-components/CRUDList.tsx
@@ -3,32 +3,40 @@ import {teacherColumns, studentColumns, parentsColumn, classesColumns, coursesCo
 function CRUDList(props){
 
     let columns;
+    let content;
 
     switch(props.type){
         case "Teachers": 
             columns = teacherColumns
+            // ! - wstawić pod kazda kolumna odwolanie do async funkcji ktora bedzie zwracala dane z API!
             break;
         case "Students":
             columns = studentColumns
+
             break;
         case "Parents":
             columns = parentsColumn
+
             break
         case "Classes":
             columns = classesColumns
+
             break
         case "Courses":
             columns = coursesColumn
+
             break
     }
+
+
     return(
-        <div className="bg-white px-4 pt-3 pb-4 rounded-sm border border-gray-200 flex-1">
-            <strong className="text-gray700 fonr-medium">{props.type} List</strong>
+        <div className="bg-white px-4 pt-3 pb-4 rounded-sm border border-gray-200 flex-1 w-5/6">
+            <strong className="text-gray-700 fonr-medium">{props.type} List</strong>
             <div className="mt-3">
-                <table>
+                <table className="table-auto text-center border-sepparate bg-gray-200">
                     <tr>
                         {columns.map((column) => (
-                            <td>{column}</td>
+                            <td className="border border-slate-600 hover:bg-gray-300 p-4">{column}</td>
                         ))}
                     </tr>
                     <tbody>
